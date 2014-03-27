067b5b3c36282107def24304813fa340b82f9fa9
sergmor <sergmor@gmail.com>
2014-03-27 14:19:41 -0400

    Update AwsCredentialConstants.java
    Updating Security breach

--- elPonePelis/src/edu/columbia/cc/elPonePeli/app/AwsCredentialConstants.java
+++ elPonePelis/src/edu/columbia/cc/elPonePeli/app/AwsCredentialConstants.java
@@ -4,8 +4,9 @@ public enum AwsCredentialConstants
 {
 //	SECRET (System.getProperty("AWS_SECRET_KEY")),
 //	ACCESS (System.getProperty("AWS_ACCESS_KEY_ID"));
-	SECRET ("Aah1YgW/zDrjNGvK/z7c1wHcllTlP9+sAsBBzNj7"),
-	ACCESS ("AKIAJXGC6OSQRB644AMA");
+// Insert your keys here
+	SECRET (),
+	ACCESS ();
 	
 	private final String key;
 	
